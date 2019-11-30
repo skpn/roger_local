668a4fe4b6e720d1c3d5d28faf8acdc859734308
Simon KPENOU <sikpenou@e2r11p13.42.fr>
2019-11-30 16:46:57 +0100

    changed ip and iproute for ip

--- script.sh
+++ script.sh
@@ -24,35 +24,47 @@ sudo_gid=$(getent group sudo | cut -d ':' -f 3)
 
 adduser --gid $sudo_gid --disabled-password --gecos "" sudouser
 echo "sudouser:sudopwd" | chpasswd
+cp /etc/sudoers /etc/sudoers_cpy
+echo 'sudouser ALL=NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo
+su sudouser
 
 ################################################################################
 ### network config
 ################################################################################
 
-ipaddr=$(ifconfig enp0s3 | awk '{ if ($1 == "inet" print $2}')
-gateway=$(iproute | awk '{ if ($1=="default") print $3 }')
+ipaddr=$(ip addr show enp0s3 | awk '{ if ($1 == "inet") print $2}')
+gateway=$(ip route show default | awk '{ print $3 }')
 network_config_file=/etc/network/interfaces
 
-sed -i "s/iface enp0s3.*/auto enp0s3\\n&/" $network_config_file
-sed -i "s/enp0s3 inet dhcp/enp0s3 inet static/" $network_config_file
-echo -e "\taddress $ipaddr/30" >> $network_config_file
-echo -e "\tgateway $gateway" >> $network_config_file
+sudo sed -i "s/iface enp0s3.*/auto enp0s3\\n&/" $network_config_file
+sudo sed -i "s/enp0s3 inet dhcp/enp0s3 inet static/" $network_config_file
+sudo echo "\taddress $ipaddr/30" >> $network_config_file
+sudo echo "\tgateway $gateway" >> $network_config_file
 
 ################################################################################
 ### ssh config
 ################################################################################
 
 ssh_config_file=/etc/ssh/sshd_config
-username=$(whoami)
-if [ ! $username=="root" ]; then
-	username=$(echo /home/$username)
-fi
-filename=$username/.ssh/id_rsa
+filename=~/.ssh/id_rsa
 
-sed -i "s/#Port 22/Port 50000/" $ssh_config_file
-sed -i "s/PermitRootLogin.*/PermitRootLogin no/" $ssh_config_file
-sed -i "s/#PasswordAuthentication.*/PasswordAuthentication no/" $ssh_config_file
-sed -i "s/#PermitEmptyPassword.*/PermitEmptyPassword no/" $ssh_config_file
-sed -i "s/#PubkeyAuthentication.*/PubkeyAuthentication yes/" $ssh_config_file
-mkdir -p $filename
-ssh-keygen -q -f $filename -N ""
+sudo sed -i "s/#Port 22/Port 50000/" $ssh_config_file
+sudo sed -i "s/PermitRootLogin.*/PermitRootLogin no/" $ssh_config_file
+sudo sed -i "s/#StrictModes.*/StrictModes yes/" $ssh_config_file
+sudo sed -i "s/#PasswordAuthentication.*/PasswordAuthentication no/" $ssh_config_file
+sudo sed -i "s/#HostbasedAuthentication.*/HostbasedAuthentication no/" $ssh_config_file
+sudo sed -i "s/#ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/" $ssh_config_file
+sudo sed -i "s/#PermitEmptyPassword.*/PermitEmptyPassword no/" $ssh_config_file
+sudo sed -i "s/#PubkeyAuthentication.*/PubkeyAuthentication yes/" $ssh_config_file
+sudo sed -i "s/#UsePAM.*/UsePAM no/" $ssh_config_file
+sudo sed -i "s/UsePAM.*/UsePAM no/" $ssh_config_file
+sudo mkdir -p $filename
+sudo ssh-keygen -q -f $filename -N ""
+
+
+
+################################################################################
+### set back sudoers
+################################################################################
+
+mv /etc/sudoers_cpy /etc/sudoers
