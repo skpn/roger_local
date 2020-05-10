16a35cf8bf4eab3423e81deb46f43bdd1cbe30b3
skpn <simon.kpenou@gmail.com>
2020-05-10 21:40:16 +0200

    changed get script to find subscripts

--- get_scripts.sh
+++ get_scripts.sh
@@ -2,12 +2,11 @@
 repo="https://raw.githubusercontent.com/skpn/roger_local/master/"
 
 function get_file() {
-	file=$(find . -type f -name "$1")
-	rm -f $file
-	wget_url=$repo$file
-	wget -q -O setup/$file $wget_url
-	if [ ! -f setup/$file ]; then
-		echo -e "could not get file $file"
+	rm -f $(find . -type f -name "$1")
+	wget_url=$repo$1
+	wget -q -O setup/$1 $wget_url
+	if [ ! -f setup/$1 ]; then
+		echo -e "could not get file $1"
 		exit 1
 	fi
 }
