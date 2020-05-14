68a9884eed9c3dee648b16582f0ad0afe8b60b6f
skpn <simon.kpenou@gmail.com>
2020-05-14 16:54:58 +0200

    allowing outbound, added dns and https ports

--- setup_firewall.sh
+++ setup_firewall.sh
@@ -12,7 +12,7 @@ echo -e "\n\nsetting up firewall rules\n\n"
 ### that accept the packets we want
 
 ### create specific log file for iptables
-if [ -z "$(grep 'iptables_' /etc/rsyslog.conf)"]; then
+if [ -z "$(grep 'iptables_' /etc/rsyslog.conf)" ]; then
 	echo ":msg,contains,\"iptables_\" /var/log/iptables.log" >> /etc/rsyslog.conf
 	service rsyslog restart
 fi
@@ -37,10 +37,11 @@ sudo ip6tables -P OUTPUT ACCEPT
 
 
 ### rejecting all traffic after logging it
-sudo iptables -I INPUT -j LOG --log-level 4 --log-prefix=iptables_reject:
-sudo iptables -I OUTPUT -j LOG --log-level 4 --log-prefix=iptables_reject:
+sudo iptables -A INPUT -j LOG --log-level 4 --log-prefix=iptables_input_reject:
 sudo iptables -A INPUT -j REJECT
+sudo iptables -A OUTPUT -j LOG --log-level 4 --log-prefix=iptables_output_reject:
 sudo iptables -A OUTPUT -j REJECT
+sudo iptables -A FORWARD -j LOG --log-level 4 --log-prefix=iptables_forward_reject:
 sudo iptables -A FORWARD -j REJECT
 
 sudo ip6tables -A INPUT -j REJECT
@@ -48,34 +49,38 @@ sudo ip6tables -A OUTPUT -j REJECT
 sudo ip6tables -A FORWARD -j REJECT
 
 
-### accept icmp protocol outbound traffic
+### accept 5 icmp protocol outbound/inbound ping packets per second up to 1000
+### packets
 sudo iptables -I OUTPUT -p icmp -m icmp --icmp-type echo-request -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
 sudo iptables -I OUTPUT -p icmp -m icmp --icmp-type echo-reply -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
 
-### accept 5 icmp protocol inbound packets per second up to 1000 packets
 sudo iptables -I INPUT -p icmp -m icmp --icmp-type echo-request -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
 sudo iptables -I INPUT -p icmp -m icmp --icmp-type echo-reply -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
 
-### accept 1 connection attempt to the ssh (50000), http (80), and smtp (25)
+
+### accept 1 connection attempt to the ssh (50000), http[s] (80,[443]), and smtp (25)
 ### ports per second up to 120 attempts
-sudo iptables -I INPUT -p tcp -m multiport --dports 50000,80,25 -m conntrack --ctstate NEW -m limit --limit 1/s --limit-burst 300 -j LOG --log-level 4 --log-prefix=iptables_new:
-sudo iptables -I INPUT -p tcp -m multiport --dports 50000,80,25 -m conntrack --ctstate NEW -m limit --limit 1/s --limit-burst 300 -j ACCEPT
+sudo iptables -I INPUT -p tcp -m multiport --dports 50000,80,443,25 -m conntrack ! --ctstate INVALID --ctstate NEW -m limit --limit 1/s --limit-burst 300 -j ACCEPT
+sudo iptables -I INPUT -p tcp -m multiport --dports 50000,80,443,25 -m conntrack ! --ctstate INVALID --ctstate NEW -m limit --limit 1/s --limit-burst 300 -j LOG --log-level 4 --log-prefix=iptables_new:
 
-### accept loopback packets and excluding packets from loopback from a
-### different machine
+### accept loopback packets
 ip_addr=$(ip addr show enp0s3 | awk '{ if ($1 == "inet") print $2}')
-sudo iptables -I INPUT -i lo -s $ip_addr -j ACCEPT
+sudo iptables -I INPUT -i lo -s 127.0.0.1 -j ACCEPT
 
-### accept established and related connections
-sudo iptables -I INPUT  -p tcp -m multiport --dports 50000,80,25 -m conntrack --ctstate ESTABLISHED,RELATED -j LOG --log-level 4 --log-prefix=iptables_est_rel:
-sudo iptables -I INPUT  -p tcp -m multiport --dports 50000,80,25 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+### accept outbound traffic
+sudo iptables -I OUTPUT -j ACCEPT
 
-### accept all output traffic from the ssh, http, and smtp ports
-sudo iptables -I OUTPUT -p tcp -m multiport --dports 50000,80,25 -j ACCEPT
+### accept established/related inbound traffic to the the ssh, http[s], and smtp ports
+#sudo iptables -I INPUT  -p tcp -m multiport --dports 50000,80,443,25 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -I INPUT -p tcp -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -I INPUT -p tcp -m conntrack --ctstate ESTABLISHED,RELATED -j LOG --log-level 4 --log-prefix=iptables_er_accept:
 
-sudo iptables -I INPUT -j LOG --log-level 4 --log-prefix=iptables_input:
-sudo iptables -I OUTPUT -j LOG --log-level 4 --log-prefix=iptables_output:
 
+
+
+##############LOG FOR DEBUG
+##############LOG FOR DEBUG
+##############LOG FOR DEBUG
 ### making iptable configuration persistent
 sudo iptables-save > /etc/iptables/rules.v4
 sudo ip6tables-save > /etc/iptables/rules.v6
