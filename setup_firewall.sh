7e77e565afc22fa239547f49582287454cb7b6c6
skpn <simon.kpenou@gmail.com>
2020-05-15 15:40:49 +0200

    switched to append for clarity

--- setup_firewall.sh
+++ setup_firewall.sh
@@ -35,52 +35,47 @@ sudo ip6tables -P INPUT ACCEPT
 sudo ip6tables -P FORWARD DROP
 sudo ip6tables -P OUTPUT ACCEPT
 
+### the rules appear in priority order:
+### - accept trusted inbound traffic ; 
+### - accept all outbound traffic ;
+### - accept 5 inbound and 5 outbound icmp packets per secound ;
 
-### rejecting all traffic after logging it
-sudo iptables -A INPUT -j LOG --log-level 4 --log-prefix=iptables_input_reject:
+### accept established/related inbound traffic to the the ssh, http[s], and smtp ports
+#sudo iptables -I INPUT  -p tcp -m multiport --dports 50000,80,443,25 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+### accept outbound traffic
+sudo iptables -A OUTPUT -j ACCEPT
+
+### accept 5 outbound icmp ping packets per second up to 1000 packets
+sudo iptables -A OUTPUT -p icmp -m icmp --icmp-type echo-request -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
+sudo iptables -A INPUT -p icmp -m icmp --icmp-type echo-reply -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
+
+### reject invalid packets
+sudo iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j REJECT
+sudo iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j REJECT
+sudo iptables -t mangle -A PREROUTING -p tcp -m connlimit --connlimit-above 50 -j REJECT
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j REJECT
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j REJECT
+sudo iptables -t mangle -A PREROUTING -f -j REJECT
+
+### accept loopback packets
+sudo iptables -A INPUT -i lo -j ACCEPT
+
+### accept 1 connection attempt to the ssh (50000), http[s] (80,[443]), and smtp (25)
+### ports per second up to 120 attempts
+sudo iptables -A INPUT -p tcp -m multiport --dports 50000,80,443,25 -m conntrack --ctstate NEW -m limit --limit 4/s --limit-burst 300 -j ACCEPT
+
+
+### rejecting all remaining traffic after logging it
+sudo iptables -A INPUT -m limit --limit 20/minute --limit-burst 60 -j LOG --log-level 4 --log-prefix=iptables_input_reject:
 sudo iptables -A INPUT -j REJECT
-sudo iptables -A OUTPUT -j LOG --log-level 4 --log-prefix=iptables_output_reject:
-sudo iptables -A OUTPUT -j REJECT
-sudo iptables -A FORWARD -j LOG --log-level 4 --log-prefix=iptables_forward_reject:
 sudo iptables -A FORWARD -j REJECT
 
 sudo ip6tables -A INPUT -j REJECT
 sudo ip6tables -A OUTPUT -j REJECT
 sudo ip6tables -A FORWARD -j REJECT
 
-
-### accept 5 icmp protocol outbound/inbound ping packets per second up to 1000
-### packets
-sudo iptables -I OUTPUT -p icmp -m icmp --icmp-type echo-request -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
-sudo iptables -I OUTPUT -p icmp -m icmp --icmp-type echo-reply -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
-
-sudo iptables -I INPUT -p icmp -m icmp --icmp-type echo-request -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
-sudo iptables -I INPUT -p icmp -m icmp --icmp-type echo-reply -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
-
-
-### accept 1 connection attempt to the ssh (50000), http[s] (80,[443]), and smtp (25)
-### ports per second up to 120 attempts
-sudo iptables -I INPUT -p tcp -m multiport --dports 50000,80,443,25 -m conntrack ! --ctstate INVALID --ctstate NEW -m limit --limit 1/s --limit-burst 300 -j ACCEPT
-sudo iptables -I INPUT -p tcp -m multiport --dports 50000,80,443,25 -m conntrack ! --ctstate INVALID --ctstate NEW -m limit --limit 1/s --limit-burst 300 -j LOG --log-level 4 --log-prefix=iptables_new:
-
-### accept loopback packets
-ip_addr=$(ip addr show enp0s3 | awk '{ if ($1 == "inet") print $2}')
-sudo iptables -I INPUT -i lo -s 127.0.0.1 -j ACCEPT
-
-### accept outbound traffic
-sudo iptables -I OUTPUT -j ACCEPT
-
-### accept established/related inbound traffic to the the ssh, http[s], and smtp ports
-#sudo iptables -I INPUT  -p tcp -m multiport --dports 50000,80,443,25 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -I INPUT -p tcp -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -I INPUT -p tcp -m conntrack --ctstate ESTABLISHED,RELATED -j LOG --log-level 4 --log-prefix=iptables_er_accept:
-
-
-
-
-##############LOG FOR DEBUG
-##############LOG FOR DEBUG
-##############LOG FOR DEBUG
 ### making iptable configuration persistent
 sudo iptables-save > /etc/iptables/rules.v4
 sudo ip6tables-save > /etc/iptables/rules.v6
