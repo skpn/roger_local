5e12ae0209279b33783f994dd0831cc2896850a0
skpn <simon.kpenou@gmail.com>
2020-05-22 18:38:34 +0200

    switched to DROP to get closed status on nmap checks

--- setup_firewall.sh
+++ setup_firewall.sh
@@ -8,11 +8,10 @@ echo -e "\n\nsetting up firewall rules\n\n"
 ### the rules at the start of the chain are consulted first, so they have higher
 ### priority
 ### inserting a rule puts it at the start of a chain, appending it at the end
-### we start by appending rules that reject all traffic ; then we insert rules
-### that accept the packets we want
+### we start by appending rules that DROP all traffic ; then we insert rules
+### that ACCEPT the packets we want
 ### necessary ports: 21 (FTP), 25 (SMTP), 53 (DNS), 80 (HTTP), 443 (HTTPS),
 ### 50000 (custom SSH)
-multiports="-m multiport --dports 21,25,53,80,443,50000"
 
 ### create specific log file for iptables
 if [ -z "$(grep 'iptables_' /etc/rsyslog.conf)" ]; then
@@ -38,23 +37,23 @@ sudo ip6tables -P INPUT ACCEPT
 sudo ip6tables -P FORWARD DROP
 sudo ip6tables -P OUTPUT ACCEPT
 
-### the rules appear in priority order:
-### - accept trusted inbound traffic ; 
-### - accept all outbound traffic ;
-### - accept 5 inbound and 5 outbound icmp packets per secound ;
+multiports="-m multiport --dports 21,25,53,80,443,50000"
 
-### accept established/related inbound traffic to the necessary ports
+### the rules appear in priority order:
+### - ACCEPT trusted inbound traffic ; 
+### - ACCEPT all outbound traffic ;
+### ACCEPT established/related inbound traffic to the necessary ports
 sudo iptables -A INPUT -p tcp $multiports -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -p udp $multiports -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-### accept outbound traffic
+### ACCEPT outbound traffic
 sudo iptables -A OUTPUT -j ACCEPT
 
-### accept 5 outbound icmp ping packets per second up to 1000 packets
+### ACCEPT 5 outbound icmp ping packets per second up to 1000 packets
 sudo iptables -A OUTPUT -p icmp -m icmp --icmp-type echo-request -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
 sudo iptables -A INPUT -p icmp -m icmp --icmp-type echo-reply -m limit --limit 5/s --limit-burst 1000 -j ACCEPT
 
-### reject invalid packets
+### DROP invalid packets
 sudo iptables -t mangle -A PREROUTING -m connlimit --connlimit-above 50 -j DROP
 sudo iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
 sudo iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
@@ -73,26 +72,26 @@ sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
 sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
 sudo iptables -t mangle -A PREROUTING -f -j DROP
 
-### accept loopback packets
+### ACCEPT loopback packets
 loopback_addr=$(ip addr show lo | awk '{ if ($1 == "inet") print $2}')
 sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A INPUT ! -i lo -s $loopback_addr -j REJECT
+sudo iptables -A INPUT ! -i lo -s $loopback_addr -j DROP
 
-### accept 1 connection attempt to the ssh (50000), http[s] (80,[443]), and smtp (25)
+### ACCEPT 1 connection attempt to the ssh (50000), http[s] (80,[443]), and smtp (25)
 ### ports per second up to 120 attempts
-sudo iptables -A INPUT -p tcp $multiports -m conntrack --ctstate NEW -m limit --limit 4/s --limit-burst 300 -j LOG --log-prefix=iptables_accept_new:
+sudo iptables -A INPUT -p tcp $multiports -m conntrack --ctstate NEW -m limit --limit 4/s --limit-burst 300 -j LOG --log-prefix=iptables_ACCEPT_new:
 sudo iptables -A INPUT -p tcp $multiports -m conntrack --ctstate NEW -m limit --limit 4/s --limit-burst 300 -j ACCEPT
 sudo iptables -A INPUT -p udp $multiports -m conntrack --ctstate NEW -m limit --limit 4/s --limit-burst 300 -j ACCEPT
 
 
-### rejecting all remaining traffic after logging it
-sudo iptables -A INPUT -m limit --limit 20/minute --limit-burst 60 -j LOG --log-level 4 --log-prefix=iptables_reject:
-sudo iptables -A INPUT -j REJECT
-sudo iptables -A FORWARD -j REJECT
+### LOG then DROP all remaining traffic
+sudo iptables -A INPUT -m limit --limit 20/minute --limit-burst 60 -j LOG --log-level 4 --log-prefix=iptables_DROP:
+sudo iptables -A INPUT -j DROP
+sudo iptables -A FORWARD -j DROP
 
-sudo ip6tables -A INPUT -j REJECT
-sudo ip6tables -A OUTPUT -j REJECT
-sudo ip6tables -A FORWARD -j REJECT
+sudo ip6tables -A INPUT -j DROP
+sudo ip6tables -A OUTPUT -j DROP
+sudo ip6tables -A FORWARD -j DROP
 
 ### making iptable configuration persistent
 sudo iptables-save > /etc/iptables/rules.v4
