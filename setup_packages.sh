6ca9bf48457c08fad1d9d13fd2d20544fab4a422
skpn <simon.kpenou@gmail.com>
2020-05-25 19:15:44 +0200

    switched to easy packages instead of iptables

--- setup_packages.sh
+++ setup_packages.sh
@@ -11,10 +11,15 @@ sed -i 's/^deb cdrom/# deb cdrom/g' /etc/apt/sources.list
 
 ###
 echo -e "pre-setting options for package installation"
-echo -e "iptables-persistent iptables-persistent/autosave_v4 boolean "\
+
+#echo -e "pre-setting options for package installation"
+#echo -e "iptables-persistent iptables-persistent/autosave_v4 boolean "\
 	"true" | debconf-set-selections
-echo -e "iptables-persistent iptables-persistent/autosave_v6 boolean "\
+#echo -e "iptables-persistent iptables-persistent/autosave_v6 boolean "\
 	"true" | debconf-set-selections
+
+debconf --priority=critical iptables-persistent
+debconf --priority=critical portsentry
 
 ###
 echo -e "updating sources, upgrading, installing necessary packages"
@@ -24,9 +29,12 @@ apt -y -qq upgrade
 apt -y -qq autoremove
 
 apt -y -qq install sudo
-apt -y -qq install iptables-persistent
 apt -y -qq install git
 apt -y -qq install vim
-apt -y -qq install incron
+apt -y -qq install ufw
+apt -y -qq install fail2ban
+apt -y -qq install portsentry
 apt -y -qq install ssh
+#apt -y -qq install iptables-persistent
+apt -y -qq install incron
 apt -y -qq install mailutils
