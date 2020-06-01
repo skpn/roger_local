8927412b53de23c5534e42b610d16ef98a332708
skpn <simon.kpenou@gmail.com>
2020-06-01 17:46:05 +0200

    created setup folder

--- setup/setup_services.sh
+++ setup/setup_services.sh
@@ -5,12 +5,6 @@
 
 echo -e "\n\ndisabling unused services\n\n"
 
-function disable_service() {
-	systemctl stop $1@.service
-	systemctl disable $1@.service
-	systemctl mask $1@.service
-}
-
 # console-setup specifies the encoding and font of the console, among other
 # things
 # keyboard-setup specifies keyboard configuration to activate the dead keys,
@@ -20,7 +14,9 @@ function disable_service() {
 
 echo -e "disabling console-setup, keyboard-setup, and bluetooth"
 
-disable_service autovt@
-disable_service console-setup
-disable_service keyboard-setup
-disable_service bluetooth
+service apache-htcacheclean stop
+service apache2 stop
+service bluetooth stop
+service console-setup stop
+service hwclock.sh stop
+service keyboard-setup stop
